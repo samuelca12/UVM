67ae2e7463d3f6728aa2cefea1e38ddde8c8c490
samuelca12 <samucabrera2002@gmail.com>
2024-09-05 08:01:47 -0600

    correct

--- agent.sv
+++ agent.sv
@@ -1,19 +1,17 @@
 class agent #(parameter width = 16, parameter depth = 8);
   comando_test_agent_mbx test_agent_mbx; // Mailbox del agente al driver
+  trans_fifo_mbx agnt_drv_mbx;
   int num_transacciones; // Número de transacciones para las funciones del agente
   int max_retardo;
   int ret_spec;
   tipo_trans tpo_spec;
+  instrucciones_agente instruccion;
   bit [width-1:0] dto_spec; // para guardar la última instrucción leída
   trans_fifo #( .width(width)) transaccion;
 
-  // Constructor
   function new;
     num_transacciones = 2;
     max_retardo = 10;
-
-    // Inicialización del mailbox
-    test_agent_mbx = new();  // Asegúrate de que esté listo para su uso
   endfunction
 
   task run;
@@ -22,41 +20,49 @@ class agent #(parameter width = 16, parameter depth = 8);
       #1;
       if (test_agent_mbx.num() > 0) begin
         $display("[%g] Agente: se recibe instrucción", $time);
-        test_agent_mbx.get(instruccion); // Usando el mailbox correcto
+        test_agent_mbx.get(instruccion);
         case (instruccion)
-          llenado_aleatorio: begin // Genera escrituras y lecturas
+          llenado_aleatorio: begin // Esta instrucción genera num_transacciones escrituras seguidas del mismo número de lecturas
             for (int i = 0; i < num_transacciones; i++) begin
               transaccion = new;
               transaccion.max_retardo = max_retardo;
               transaccion.randomize();
-              transaccion.tipo = escritura; // Escritura
-              transaccion.print("Agente: Escritura generada");
-              test_agent_mbx.put(transaccion);
+              transaccion.tipo = escritura;
+              transaccion.print("Agente: transacción creada");
+              agnt_drv_mbx.put(transaccion);
             end
             for (int i = 0; i < num_transacciones; i++) begin
               transaccion = new;
               transaccion.randomize();
-              transaccion.tipo = lectura; // Lectura
-              transaccion.print("Agente: Lectura generada");
-              test_agent_mbx.put(transaccion);
+              transaccion.tipo = lectura;
+              transaccion.print("Agente: transacción creada");
+              agnt_drv_mbx.put(transaccion);
             end
           end
-          trans_especifica: begin // Transacción específica de escritura o lectura
+          trans_aleatoria: begin
             transaccion = new;
-            transaccion.tipo = tpo_spec; // Puede ser lectura o escritura
+            transaccion.max_retardo = max_retardo;
+            transaccion.randomize();
+            transaccion.print("Agente: transacción creada");
+            agnt_drv_mbx.put(transaccion);
+          end
+
+          trans_especifica: begin
+            transaccion = new;
+            transaccion.tipo = tpo_spec;
             transaccion.dato = dto_spec;
             transaccion.retardo = ret_spec;
-            transaccion.print("Agente: Transacción específica");
-            test_agent_mbx.put(transaccion);
+            transaccion.print("Agente: transacción creada");
+            agnt_drv_mbx.put(transaccion);
           end
-          sec_trans_aleatorias: begin // Genera secuencia de transacciones aleatorias
+
+          sec_trans_aleatorias: begin // Esta instrucción genera una secuencia de instrucciones aleatorias
             for (int i = 0; i < num_transacciones; i++) begin
               transaccion = new;
               transaccion.max_retardo = max_retardo;
               transaccion.randomize();
-              transaccion.tipo = (i % 2 == 0) ? escritura : lectura; // Alterna escritura y lectura
-              transaccion.print("Agente: Transacción aleatoria");
-              test_agent_mbx.put(transaccion);
+              transaccion.print("Agente: transacción creada");
+              agnt_drv_mbx.put(transaccion);
             end
           end
         endcase
